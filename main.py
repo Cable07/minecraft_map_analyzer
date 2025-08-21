2b42423f30768c67ef5072b8c537d035ea596cd5
Cable07 <cstockton221@gmail.com>
2025-08-21 12:18:06 -0500

    Update main.py
    Still trying to grasp the whole concept of using anvil-parser

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("testing\n")
+import anvil
+
+region = anvil.Region.from_file("C:/Users/cstoc/AppData/Roaming/.minecraft/saves/MapDataTest/region/r.0.0.mca")
+
+chunk = anvil.Chunk.from_region(region, 0, 0)
+
+block = chunk.get_block(0, 49, 0)
+
+print(block) # <Block(minecraft:air)>
+print(block.id) # air
+print(block.properties) # {}
