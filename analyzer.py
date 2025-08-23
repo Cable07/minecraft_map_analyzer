bb7e82f6f421469d15f38e7740f57d5b767cb12a
Cable07 <cstockton221@gmail.com>
2025-08-22 22:09:25 -0500

    Parser Files
    Changed main.py to analyzer.py and added parser files

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,9 @@ import anvil
 
 region = anvil.Region.from_file("C:/Users/cstoc/AppData/Roaming/.minecraft/saves/MapDataTest/region/r.0.0.mca")
 
-chunk = anvil.Chunk.from_region(region, 0, 0)
+chunk = anvil.Chunk.from_region(region, 22, 27)
 
-block = chunk.get_block(0, 49, 0)
+block = chunk.get_block(5, 65, 8)
 
 print(block) # <Block(minecraft:air)>
 print(block.id) # air
