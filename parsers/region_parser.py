d47a7bc6f9cce514fcffedd5f7f1dc7db74c497c
Cable07 <cstockton221@gmail.com>
2025-08-23 13:50:13 -0500

    Update region_parser.py
    Region parser option 2 is still not functional. Although I added the optics around it to grab inputs from user and analyze the corner blocks only.

--- parsers/region_parser.py
+++ parsers/region_parser.py
@@ -16,12 +16,13 @@ REQUIRED REGION FILES AT COORDS -> r.0.0.mca , r.-2.-2.mca
 REQUIRED REGION FILES IN BETWEEN -> r.0.-1.mca , r.0.-2.mca , r.-1.0.mca , r.-1.-1.mca , r.-1.-2.mca , r.-2.0.mca , r.-2.-1.mca
 """
 
-
-file_path = "C:/Users/cstoc/AppData/Roaming/.minecraft/saves/MapDataTest/region/r."
 Option = input("This is the block parser. \nEnter 1 if you would you like what kind of block is at a particular coordinate or enter 2 if you would like to know the block data for an area.\n")
 
+world_name = input("\nWhat is the name of your world?\n")
+
+file_path = "C:/Users/cstoc/AppData/Roaming/.minecraft/saves/" + world_name + "/region/r."
+
 if Option == "1":
-    print("Great Choice, ez \n")
     x_coord = int(input("x = "))
     y_coord = int(input("y = "))
     z_coord = int(input("z = "))
@@ -51,12 +52,63 @@ if Option == "1":
 
     block = chunk.get_block(local_x_coord, y_coord, local_z_coord)
 
-    #print(block) # <Block(minecraft:air)>
     print(block.id) # air
-    #print(block.properties) # {}
-    
 
 elif Option == "2":
-    print("Yikerz")
+    print("I'll do my best :(")
+    x1 = int(input("\n1st x = "))
+    y1 = int(input("\n1st y = "))
+    z1 = int(input("\n1st z = "))
+
+    x2 = int(input("\n\n2nd x = "))
+    y2 = int(input("\n2nd y = "))
+    z2 = int(input("\n2nd z = "))
+
+    #normalize inputs so that smaller numbers are always assigned to x1, y1, z1
+    #x1, x2 = min(x1, x2), max(x1, x2)
+    ##z1, z2 = min(z1, z2), max(z1, z2)
+
+    #calculates the region values for each coord
+    region_x1 = str(x1 // 512)
+    region_z1 = str(z1 // 512)
+    region_x2 = str(x2 // 512)
+    region_z2 = str(z2 // 512)
+
+    region1 = region_x1 + "." + region_z1 + ".mca"
+    region2 = region_x2 + "." + region_z2 + ".mca"
+
+    #calculates the global chunk value
+    global_chunk_x1 = x1 // 16
+    global_chunk_z1 = z1 // 16
+    global_chunk_x2 = x2 // 16
+    global_chunk_z2 = z2 // 16
+
+    #calculates the local chunk value within the region file
+    local_chunk_x1 = global_chunk_x1 % 32
+    local_chunk_z1 = global_chunk_z1 % 32
+    local_chunk_x2 = global_chunk_x2 % 32
+    local_chunk_z2 = global_chunk_z2 % 32
+
+    local_x1 = x1 % 16
+    local_z1 = z1 % 16
+    local_x2 = x2 % 16
+    local_z2 = z2 % 16
+
+    region1 = anvil.Region.from_file(file_path + region1)
+
+    chunk1 = anvil.Chunk.from_region(region1, local_chunk_x1, local_chunk_z1)
+
+    block1 = chunk1.get_block(local_x1, y1, local_z1)
+
+    region2 = anvil.Region.from_file(file_path + region2)
+
+    chunk2 = anvil.Chunk.from_region(region2, local_chunk_x2, local_chunk_z2)
+
+    block2 = chunk2.get_block(local_x2, y2, local_z2)
+
+    print("\n\nBlock 1 is : " + block1.id)
+    print("\nBlock 2 is : " + block2.id)
+
+
 else:
     print("YOU WRONG")
