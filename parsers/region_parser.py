bb9fcade489527e23431f944c2ca08cbe33e7cab
Cable07 <cstockton221@gmail.com>
2025-08-24 15:43:02 -0500

    Update region_parser.py
    Partial functionality (says name of each block but does not count them)
    
    NOTE: The parser works north to south, bottom to top, then west to east

--- parsers/region_parser.py
+++ parsers/region_parser.py
@@ -27,14 +27,14 @@ def analyze_block(x, y, z):
 
     region = region_x + "." + region_z + ".mca"
 
-    global_chunk_x = (x_coord // 16)
-    global_chunk_z = (z_coord // 16)
+    global_chunk_x = (x // 16)
+    global_chunk_z = (z // 16)
 
     local_chunk_x = (global_chunk_x % 32)
     local_chunk_z = (global_chunk_z % 32)
 
-    local_x_coord = (x_coord % 16)
-    local_z_coord = (z_coord % 16)
+    local_x_coord = (x % 16)
+    local_z_coord = (z % 16)
 
     region = anvil.Region.from_file(file_path + region)
 
@@ -68,52 +68,23 @@ elif Option == "2":
     z2 = int(input("\n2nd z = "))
 
     #normalize inputs so that smaller numbers are always assigned to x1, y1, z1
-    #x1, x2 = min(x1, x2), max(x1, x2)
-    ##z1, z2 = min(z1, z2), max(z1, z2)
+    x1, x2 = min(x1, x2), max(x1, x2)
+    y1, y2 = min(y1, y2), max(y1, y2)
+    z1, z2 = min(z1, z2), max(z1, z2)
 
-    analyze_block(x1, y1, z1)
-    analyze_block(x2, y2, z2)
+    og_y = y1
+    og_z = z1
 
-    #calculates the region values for each coord
-    region_x1 = str(x1 // 512)
-    region_z1 = str(z1 // 512)
-    region_x2 = str(x2 // 512)
-    region_z2 = str(z2 // 512)
-
-    region1 = region_x1 + "." + region_z1 + ".mca"
-    region2 = region_x2 + "." + region_z2 + ".mca"
-
-    #calculates the global chunk value
-    global_chunk_x1 = x1 // 16
-    global_chunk_z1 = z1 // 16
-    global_chunk_x2 = x2 // 16
-    global_chunk_z2 = z2 // 16
-
-    #calculates the local chunk value within the region file
-    local_chunk_x1 = global_chunk_x1 % 32
-    local_chunk_z1 = global_chunk_z1 % 32
-    local_chunk_x2 = global_chunk_x2 % 32
-    local_chunk_z2 = global_chunk_z2 % 32
-
-    local_x1 = x1 % 16
-    local_z1 = z1 % 16
-    local_x2 = x2 % 16
-    local_z2 = z2 % 16
-
-    region1 = anvil.Region.from_file(file_path + region1)
-
-    chunk1 = anvil.Chunk.from_region(region1, local_chunk_x1, local_chunk_z1)
-
-    block1 = chunk1.get_block(local_x1, y1, local_z1)
-
-    region2 = anvil.Region.from_file(file_path + region2)
-
-    chunk2 = anvil.Chunk.from_region(region2, local_chunk_x2, local_chunk_z2)
-
-    block2 = chunk2.get_block(local_x2, y2, local_z2)
-
-    print("\n\nBlock 1 is : " + block1.id)
-    print("\nBlock 2 is : " + block2.id)
+    print(x1, x2, y1, y2, z1, z2)
+    while(x1 <= x2):
+        while(y1 <= y2):
+            while(z1 <= z2):
+                analyze_block(x1, y1, z1)
+                z1+=1
+            y1+=1
+            z1 = og_z
+        x1+=1
+        y1 = og_y
 
 
 else:
